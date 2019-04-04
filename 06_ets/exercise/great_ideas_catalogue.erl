bba46a3ca16d89ab338378eb98aeda59735fe0ef
Igor Kavzov <igor.kavzov@gmail.com>
2019-04-04 22:46:31 +0300

    Complete tasks

--- 06_ets/exercise/great_ideas_catalogue.erl
+++ 06_ets/exercise/great_ideas_catalogue.erl
@@ -5,13 +5,14 @@
 -export([init/0,
          add_idea/5, get_idea/1,
          ideas_by_author/1, ideas_by_rating/1,
-         get_authors/0]).
+         get_authors/0,
+         elem_cnt/2]).
 
 -record(idea, {id, title, author, rating, description}).
 
 
 init() ->
-    ets:new(great_ideas_table, [set, named_table, {keypos, 2}]),
+    T = ets:new(great_ideas_table, [set, named_table, {keypos, 2}]),
     ets:insert(great_ideas_table,
                [#idea{id = 1, title = "Мороженое с огурцами", author = "Боб Бобов", rating = 100,
                       description = "Крошим огурцы кубиками и добавляем в мороженое"},
@@ -33,24 +34,57 @@ init() ->
                       description = "Нужно использовать энергию квазаров для этой цели"},
                 #idea{id = 10, title = "Самодвижущаяся лестница", author = "Васисуалий Л.", rating = 42,
                       description = "Имеет большой потенциал применения в небоскребах."}]),
-    ok.
+    T.
 
 
 add_idea(Id, Title, Author, Rating, Description) ->
+    ets:insert(great_ideas_table, {idea, Id, Title, Author, Rating, Description}),
     ok.
 
 
 get_idea(Id) ->
-    not_found.
+    Res = ets:lookup(great_ideas_table, Id),
+    if
+        Res == [] -> not_found;
+        true -> [Idea] = Res, {ok, Idea}
+    end.
 
 
 ideas_by_author(Author) ->
-    [].
+    lists:sort(ets:match_object(great_ideas_table, {idea, '_', '_', Author, '_', '_'})).
 
 
 ideas_by_rating(Rating) ->
-    [].
+    MS = ets:fun2ms(
+        fun({idea, '_', '_', '_', IdeaRating, '_'} = Idea) when IdeaRating >= Rating ->
+            Idea
+        end
+    ),
+    ets:select(great_ideas_table, MS).
+
+
+elem_cnt(Elem, List) ->
+    Cnt = lists:foldl(
+        fun(Item, Cnt) ->
+            if Item == Elem -> Cnt+1;
+            true -> Cnt
+            end
+        end,
+        0,
+        List
+    ),
+    {Elem, Cnt}.
 
 
 get_authors() ->
-    [].
+    MS = ets:fun2ms(fun({idea, '_', '_', Author, '_', '_'}) -> Author end),
+    Authors = ets:select(great_ideas_table, MS),
+    NumAuthorsIdeas = lists:map(fun(A) -> elem_cnt(A, Authors) end, lists:usort(Authors)),
+    SortFn = 
+        fun({A1, C1},{A2, C2}) ->
+            if C1 == C2 -> A2 > A1;
+            true -> C1 > C2
+            end
+        end,
+    lists:sort(SortFn, NumAuthorsIdeas).
+
