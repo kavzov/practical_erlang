70d6797de0bf210fb338f9adbdb124d71f180985
Igor Kavzov <igor.kavzov@gmail.com>
2019-04-06 22:22:57 +0300

    Complete binary strings task

--- 07_binary_string/exercise/template.erl
+++ 07_binary_string/exercise/template.erl
@@ -2,5 +2,49 @@
 
 -export([parse/2]).
 
-parse(Str, Data) when is_binary(Str) ->
-    Str.
+-define(StartTmpl, <<"{{">>).
+-define(EndTmpl, <<"}}">>).
+-define(EmptyStr, <<"">>).
+
+
+get_tmpl_word(Tmpl) ->
+    %% Extract {{key}} -> key
+    binary:replace(
+        binary:replace(Tmpl, ?EndTmpl, ?EmptyStr),
+        ?StartTmpl, ?EmptyStr
+    ).
+
+
+to_binary(Value) ->
+    %% Convert integer and string values to binary 
+    IsInteger = is_integer(Value),
+    IsList = is_list(Value),
+    if 
+        IsInteger -> integer_to_binary(Value);
+        IsList -> list_to_binary(Value);
+    true -> Value
+    end.
+
+
+parse(BinStr, Data) ->
+    % Get start match indexes
+    Starts = binary:matches(BinStr, ?StartTmpl),
+    % Get end match indexes
+    Ends = binary:matches(BinStr, ?EndTmpl),
+    % Get {start, length} tuples list to extract template words (for binary_part function)
+    TmplIdxs = lists:map(fun(T)-> {A,B}=T, {A1,_}=A, {B1,B2}=B, {A1,B1+B2-A1} end, lists:zip(Starts, Ends)),
+    % Get template words with {{ }} from BinStr. They will be replaced with its Data values.
+    TmplWords = lists:map(
+        fun(Idxs) -> binary_part(BinStr, Idxs) end,
+        TmplIdxs
+    ),
+    % Replace the BinStr template words with its Data values
+    lists:foldl(
+        fun(Word, Str) -> 
+            DataValue = to_binary(maps:get(get_tmpl_word(Word), Data, ?EmptyStr)),
+            binary:replace(Str, Word, DataValue)
+        end,
+        BinStr,
+        TmplWords
+    ).
+
