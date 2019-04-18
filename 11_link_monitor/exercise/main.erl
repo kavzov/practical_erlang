305305d558d7e1e8afd27cd4e85b3b7e32ff9e2b
Igor Kavzov <igor.kavzov@gmail.com>
2019-04-18 12:58:16 +0300

    Complete task 11

--- 11_link_monitor/exercise/main.erl
+++ 11_link_monitor/exercise/main.erl
@@ -4,4 +4,38 @@
 
 
 parse(Files) ->
-    ok.
+    error_logger:tty(false),  % suppress = ERROR REPORT ====
+    lists:foldl(
+        fun(File, {Acc, Err}) ->
+            Pid = spawn(worker, parse_file, []),
+            Ref = erlang:monitor(process, Pid),
+            Pid ! {self(), Ref, File},
+            receive
+                {reply, Ref, Reply} ->
+                    erlang:demonitor(Ref, [flush]),
+                    {aggregate(Acc, Reply), Err};
+                {'DOWN', Ref, process, _Pid, Reason} ->
+                    erlang:demonitor(Ref, [flush]),
+                    {Acc, Err#{File => Reason}}
+            after 5000 ->
+                erlang:demonitor(Ref, [flush]),
+                {error, no_reply}
+            end
+        end,
+        {#{}, #{}},
+        Files
+    ).
+
+
+aggregate(Map1, Map2) ->
+    maps:fold(
+        fun(K, V, Acc) ->
+            case maps:find(K, Acc) of
+                {ok, Val} -> Acc#{K => V + Val};
+                error     -> Acc#{K => V}
+            end
+        end,
+        Map1,
+        Map2
+    ).
+
--- 11_link_monitor/exercise/worker.erl
+++ 11_link_monitor/exercise/worker.erl
@@ -1,1 +1,27 @@
 -module(worker).
+
+-export([parse_file/0]).
+
+
+parse_file() ->
+    receive
+        {From, Ref, File} ->
+            {ok, Data} = file:read_file(File),
+            Records = binary:split(Data, [<<"\n">>, <<"\r">>], [global]),
+            Records2 = lists:filter(fun(Item) -> Item =/= <<>> end, Records),
+            Parsed_data = lists:foldl(
+                fun(Rec, Acc) ->
+                    [_, Name, Count, _] = binary:split(Rec, [<<",">>], [global]),
+                    IntCount = list_to_integer(binary_to_list(Count)),
+                    case maps:find(Name, Acc) of
+                        {ok, CurrCount} -> Acc#{Name := CurrCount + IntCount};
+                        error -> Acc#{Name => IntCount}
+                    end
+                end,
+                #{},
+                Records2
+            ),
+            From ! {reply, Ref, Parsed_data}
+    after 3000 ->
+        no_file_to_parse
+    end.
